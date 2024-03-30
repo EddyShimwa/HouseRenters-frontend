9e6b54a59df9af25b041d71f9f4f6cdc808bf6b4
EddyShimwa <shedpray16@gmail.com>
2024-03-30 21:14:26 +0200

    modify splash page

--- src/componentsFolder/splash.js
+++ src/componentsFolder/splash.js
@@ -15,25 +15,28 @@ const SplashPage = () => {
     <div 
       className="splash-page h-screen flex flex-col items-center justify-center "
     >
-  <div className="flex items-center mb-2">
-  <h2 className="text-6xl text-white mr-1 font-bold mt-8 ">SCHOLARZ</h2>
-  <img src={logo} alt="Logo" className="mx-2" />
-  <h2 className="text-6xl text-white -ml-4 font-bold mt-8">NID</h2>
-</div>
+      <div className="flex items-center mb-2 ">
+        <h2 className="text-6xl text-white -mr-6 font-bold mt-9 ">SCHOLARZ</h2>
+        <img src={logo} alt="Logo" className="mx-2" />
+        <h2 className="text-6xl text-white -ml-8 font-bold mt-9">NID</h2>
+      </div>
       <h1 className="text-6xl text-white mb-8 font-bold">
         House Renting Made <span style={{ color: '#2dbcfa' }}>Easy</span>!
       </h1>
-      <div className="textdiv" style={{ maxWidth: '50%', margin: '0 auto' }}>
-  <h4 className="mytext">
-    Unlock a world of possibilities and convenience in house hunting with our simplified renting solutions.
-  </h4>
-</div>
+      <div className="textdiv" style={{ maxWidth: '30%', margin: '0 auto' }}>
+        <h4 className="mytext text-2xl animate-pulse">
+          Unlock a world of possibilities and convenience in house hunting with our simplified renting solutions.
+        </h4>
+      </div>
       <button 
-        className="bg-white text-blue-500 py-2 px-4 rounded-full text-xl font-bold mt-8"
+        className="transition duration-700 ease-in-out py-2 px-4 rounded-half text-xl font-bold mt-8 get-started animate-bounce"
         onClick={navigateToHome}
       >
         Let's Get Started
       </button>
+      <footer className="mt-auto text-center pb-4">
+        © {new Date().getFullYear()} SCHOLARZNID. All rights reserved.
+      </footer>
     </div>
   );
 }
